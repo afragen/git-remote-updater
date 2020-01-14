d6b30c20f47d5c4f43098040cca73cd90158103e
Andy Fragen <andy@thefragens.com>
2020-01-14 13:53:37 -0800

    cleanup and fix PHP warning

--- src/Git_Remote_Updater/Webhooks.php
+++ src/Git_Remote_Updater/Webhooks.php
@@ -228,7 +228,7 @@ trait Webhooks {
 	 * @return array
 	 */
 	private function remove_slugs_from_json( $json, $remove_slugs ) {
-		foreach ( $json as $sites ) {
+		foreach ( (array) $json as $sites ) {
 			foreach ( $sites->sites->slugs as $key => $slug ) {
 				if ( in_array( $slug->slug, $remove_slugs, true ) ) {
 					unset( $sites->sites->slugs[ $key ] );
