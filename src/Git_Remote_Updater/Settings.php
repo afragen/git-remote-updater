9a3c590fecd7c2dd60c7d3bd26ec701dbc1f9861
Andy Fragen <andy@thefragens.com>
2020-01-20 10:56:11 -0800

    oops, use precision

--- src/Git_Remote_Updater/Settings.php
+++ src/Git_Remote_Updater/Settings.php
@@ -88,7 +88,7 @@ class Settings {
 		esc_html_e( 'Git Remote Updater', 'git-remote-updater' );
 		echo '</h2>';
 
-		if ( 'git-remote-updater' == $page ) {
+		if ( 'git-remote-updater' === $page ) {
 			( new Actions() )->display( $action );
 		}
 		if ( 'git-remote-updater-settings' === $page ) {
