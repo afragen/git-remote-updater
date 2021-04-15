42f6a426eb34bf5d5dd0dcead4ecbc22ac8eb09f
Andy Fragen <andy@thefragens.com>
2021-04-15 14:41:53 -0700

    composer update

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,24 +25,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
     'aliases' => 
     array (
     ),
-    'reference' => '111d5cff4ec56e22e97c76070f11500d2f1472ad',
+    'reference' => '1f99936f49d4aef4be84fc88c44c1a9f59f6c610',
     'name' => 'afragen/git-remote-updater',
   ),
   'versions' => 
   array (
     'afragen/git-remote-updater' => 
     array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
       'aliases' => 
       array (
       ),
-      'reference' => '111d5cff4ec56e22e97c76070f11500d2f1472ad',
+      'reference' => '1f99936f49d4aef4be84fc88c44c1a9f59f6c610',
     ),
   ),
 );
