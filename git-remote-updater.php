bc4541e2b4cec0df2c65172b041d1cbff05c3c7f
Andy Fragen <andy@thefragens.com>
2022-04-24 22:47:34 -0700

    release

--- git-remote-updater.php
+++ git-remote-updater.php
@@ -14,7 +14,7 @@
  * Description:       Allows you to easily update Git Updater repositories in bulk via REST API endpoint updating. Requires Git Updater PRO.
  * Author:            Andy Fragen
  * Author URI:        https://github.com/afragen
- * Version:           2.4.3
+ * Version:           3.0.0
  * License:           MIT
  * Network:           true
  * Domain Path:       /languages
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'ab822eb23c794edd70f045e53e85c5633a4a2279',
+        'reference' => '2899b0a0a0d2194540f168dc88023cbfcea9c9c3',
         'name' => 'afragen/git-remote-updater',
         'dev' => false,
     ),
@@ -16,7 +16,7 @@
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'ab822eb23c794edd70f045e53e85c5633a4a2279',
+            'reference' => '2899b0a0a0d2194540f168dc88023cbfcea9c9c3',
             'dev_requirement' => false,
         ),
         'freemius/wordpress-sdk' => array(
