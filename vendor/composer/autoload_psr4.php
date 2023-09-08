f4c8eb774812850739a0e67318d74aeb7e83c285
Andy Fragen <andy@thefragens.com>
2023-09-08 09:04:23 -0700

    composer update, WPCS 3.0.0

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PHPCSStandards\\Composer\\Plugin\\Installers\\PHPCodeSniffer\\' => array($vendorDir . '/dealerdirect/phpcodesniffer-composer-installer/src'),
     'Fragen\\Git_Updater\\' => array($baseDir . '/src/Git_Updater'),
     'Fragen\\Git_Remote_Updater\\' => array($baseDir . '/src/Git_Remote_Updater'),
 );
